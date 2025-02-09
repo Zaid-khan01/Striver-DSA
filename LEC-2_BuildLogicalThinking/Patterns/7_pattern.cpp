f225f73446be02e9f15e0e9f861368a114f5b48e
Zaid khan <itszaidkhan01@gmail.com>
2025-02-09 20:37:23 +0530

    7-pattern

---
+++ LEC-2_BuildLogicalThinking/Patterns/7_pattern.cpp
@@ -0,0 +1,29 @@
+/*
+            *
+          * * *
+        * * * * *
+      * * * * * * *
+    * * * * * * * * *
+
+*/
+
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int i, j, n;
+    cout << "Enter a limit = ";
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        for(int k=1;k<=n-i;k++){
+            cout<<"  ";
+        }
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
