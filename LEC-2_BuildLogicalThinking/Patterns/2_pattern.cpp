53578f47cff2280fb2ae1caefcebefc3468c4678
Zaid khan <itszaidkhan01@gmail.com>
2025-02-09 20:02:56 +0530

    second-pattern

---
+++ LEC-2_BuildLogicalThinking/Patterns/2_pattern.cpp
@@ -0,0 +1,25 @@
+/*
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+ */
+
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int i, j, n;
+    cout << "Enter a limit = ";
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
