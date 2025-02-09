bc3120cd955cd6249a0b9e9a556facbc0429f257
Zaid khan <itszaidkhan01@gmail.com>
2025-02-09 20:23:01 +0530

    5-pattern

---
+++ LEC-2_BuildLogicalThinking/Patterns/5_pattern.cpp
@@ -0,0 +1,25 @@
+/*
+    * * * * *
+    * * * *
+    * * *
+    * *
+    *
+*/
+
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int i, j, n;
+    cout << "Enter limit = ";
+    cin >> n;
+    for (i = n; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
